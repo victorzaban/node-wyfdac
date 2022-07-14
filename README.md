2260418be6b522fc948e53d5f1447c9e6d66f0ae
victorzaban <victor_zaban@hotmail.com>
2022-07-14 12:42:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-wyfdac
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-wyfdac)
---
+++ index.js
@@ -0,0 +1,5 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+
+
